4325bff6bbb2da5f62a0e2f983bdadc8a32eab4c
Lucas Tabare <lucas.tabare@gmail.com>
2022-06-18 22:53:34 -0300

    se crea primer entity

---
+++ PruebaTecnica-proyecto/Models/ModelList.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace PruebaTecnica_proyecto.Models
+{
+    public class ModelList
+    {
+        public List<ResponsePais> listPais { get; set; }
+    }
+}
---
+++ PruebaTecnica-proyecto/Scripts/app/registroEmpresa.js
@@ -0,0 +1,21 @@
+﻿
+/*VISUALIZR IMAGEN LOGO*/
+$("#imagen").change(function () {
+    var imagen = this.file[0];
+    if (imagen["type"] != "image/jpg" && imagen["type"] != "image/png") {
+        $(#imagen).val("");
+        $(".previsualizar").attr("src", "../Content/img/logo.png");
+        alert("Debe Subir Una Imagen En Formato JPG O PNG");
+    } else if (imagen["size"] > 2000000) {
+        $(#imagen).val("");
+        $(".previsualizar").attr("src", "../Content/img/logo.png");
+        alert("La Imagen Debe Ser Menor A 2MB");
+    } else {
+        var datosImagen = new FileReader;
+        datosImagen.readAsDataUrl(imagen)
+        $(datosImagen).on("load", function (e) {
+            var rutaImagen = e.target.result;
+            $(".previsualizar").attr("src", rutaImagen)
+        })
+    }
+})
